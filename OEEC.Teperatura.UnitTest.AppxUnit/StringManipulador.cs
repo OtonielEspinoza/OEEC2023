7ba808f0d54b72b1125c48513183da7c9f3b483f
Otoniel Esteban Espinoza Contreras <esteban15espinosa@outlook.com>
2023-10-15 21:28:38 -0600

    Utilizamos xUnit

---
+++ OEEC.Teperatura.UnitTest.AppxUnit/StringManipulador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OEEC.Teperatura.UnitTest.AppxUnit
+{
+    public class StringManipulador
+    {
+        public string ReverseString(string input)
+        {
+
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public string RemoveSpaces(string input)
+        {
+            return input.Replace(" ", "");
+
+        }
+    }
+}
