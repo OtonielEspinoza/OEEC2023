a8ac802ed18e6197425049c23ba950c908693139
Otoniel Esteban Espinoza Contreras <esteban15espinosa@outlook.com>
2023-10-17 10:43:10 -0600

    Solo cambie el nombre del proyecto

--- OEEC.Teperatura.UnitTest.AppxUnit/StringManipuladorxTests.cs
+++ OEEC.Teperatura.UnitTest.AppxUnit/StringManipuladorxTests.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OEEC.Teperatura.UnitTest.AppxUnit
+namespace OEEC.StringManipulador.UnitTest.AppxUnit
 {
     public class StringManipulador
     {
